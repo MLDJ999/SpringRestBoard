05d1e4cdf8611e0bb23932c33c659461fa99f846
MLDJ-999 <needsoju7@gmail.com>
2024-04-03 17:49:22 +0900

    글 목록 조회버튼 클릭 시 정보 전달까지

--- SpringRestBoard/src/main/java/com/itwillbs/controller/BoardRestController.java
+++ SpringRestBoard/src/main/java/com/itwillbs/controller/BoardRestController.java
@@ -4,6 +4,10 @@ import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +18,7 @@ import com.itwillbs.domain.BoardVO;
 import com.itwillbs.service.BoardService;
 
 @RestController
+@RequestMapping(value = "/boards")
 public class BoardRestController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(BoardRestController.class);
@@ -22,17 +27,47 @@ public class BoardRestController {
 	@Inject
 	private BoardService bService;
 	
+	//http://localhost:8088/boardMain
 	//@RequestMapping(value = "/boards", method = RequestMethod.POST)
-	@PostMapping(value = "/boards")
-	public void boardAdd(@RequestBody BoardVO vo) throws Exception {
+	@PostMapping(value = "")
+	public ResponseEntity<String> boardAdd(@RequestBody BoardVO vo) throws Exception {
 		logger.info(" boardAdd() 실행! ");
 		logger.info(" vo : " + vo);
 		
+		try {
+			logger.info(" bService : " + bService);
+			bService.boardAdd(vo);
+		} catch (Exception e) {
+			return ResponseEntity.badRequest()
+					.contentType(MediaType.valueOf("text/plain; charset=UTF8"))
+					.body("글 작성 실패!");
+			
+		}
+		// return new ResponseEntity<String>("글 작성 성공!", HttpStatus.OK);
+		return ResponseEntity.ok()
+				.contentType(MediaType.valueOf("text/plain; charset=UTF8"))
+				.body("글 작성 성공!");
+		
 		// 전달받은 데이터를 저장
-		logger.info(" bService : " + bService);
-		bService.boardAdd(vo);
 		
 	}
+	
+	@RequestMapping(value = "/{bno}", method = RequestMethod.GET)
+	public void boardRead(@PathVariable("bno") int bno) {
+		logger.debug(" boardRead() 실행 ");
+		logger.info(" bno : " + bno);
+		// 특정 글 번호가 존재하는지 체크
+		
+		
+		// 전달받은 bno에 해당하는 글정보를 가져오기
+		// 만약에 해당 bno가 없는경우 가장 마지막 글 정보를 가져오기
+		
+		
+		
+	}
+	
+		
+	
 	
 	
 	
--- SpringRestBoard/src/main/webapp/WEB-INF/views/boardMain.jsp
+++ SpringRestBoard/src/main/webapp/WEB-INF/views/boardMain.jsp
@@ -34,9 +34,14 @@
 				url : "/boards",
 				type : "POST",
 				data : JSON.stringify(board),
-				contentType : "application/json",
-				success : function() {
+				contentType : "application/json; charset=UTF-8",
+				success : function(data) {
 					alert(" /boards 다녀옴! ");
+					$("#title").val("");
+					$("#writer").val("");
+					$("#content").val("");
+					
+					$("#statusH2").append(data);
 				},
 				error : function() {
 					alert(" /boards 다녀옴 실패! ");
@@ -45,14 +50,36 @@
 			
 			
 			alert("글쓰기 버튼 클릭 - 끝");
-		});
-	});
+		}); // btnAdd
+		
+		
+		
+		$("#btnRead").click(function(){
+			// 마지막 글 조회 버튼 클릭시 DB에 저장된 마지막 글 정보를 가져와서 화면에 출력
+			
+			$.ajax({
+				url : "/boards/1000",
+				type : "GET",
+				success : function() {
+					alert(" 글 조회 페이지 다녀옴 !");
+				}
+			});
+			
+		}); // btnRead
+		
+		
+		
+	}); // ready
 </script>
 </head>
+
+
+
+
 <body>
 	<h1>boardMain.jsp</h1>
 	<!-- 사용자, 제목, 내용 입력받아서 글 작성  -->
-	<h2>상태 : </h2>
+	<h2 id = "statusH2">상태 : </h2>
 	<fieldset style="width: 450px;">
 		<legend> 글쓰기 (POST) </legend>
 		작성자 : <input type="text" name="writer" id="writer"><br>
@@ -60,6 +87,13 @@
 		내용 : <br>
 		<textarea rows="5" cols="50" name="content" id="content"></textarea>
 		<br> <input type="button" value="글쓰기(POST)" id="btnAdd">
+	</fieldset>
+	
+	<hr>
+	<fieldset>
+		<legend> 글 조회(GET) </legend>
+		<input type = "button" value = "글조회(GET)" id = "btnRead">
+		
 	</fieldset>
 
 
